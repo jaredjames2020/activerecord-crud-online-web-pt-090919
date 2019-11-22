a439d180451ee96fa20c9421da5375e30472a9f8
Jared S <jaredjames2020@gmail.com>
2019-11-22 18:18:04 +0000

    Automatically backed up by Learn

--- db/migrate/20191122181557_create_movies.rb
+++ db/migrate/20191122181557_create_movies.rb
@@ -1,10 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    
-    t.string :title
-    t.integer :release_date
-    t.string :director
-    t.string :lead
-    t.boolean :in_theaters
+    create_table :create_movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
   end
 end
